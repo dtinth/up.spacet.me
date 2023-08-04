7ac44f08685e57f79919366ed6584d18e88cac6b
dtinth on MBP M1 <dtinth@spacet.me>
2023-08-05 02:45:58 +0700

    upload as webp

--- src/uppy.tsx
+++ src/uppy.tsx
@@ -4,6 +4,7 @@ import ImageEditor from "@uppy/image-editor";
 import ScreenCapture from "@uppy/screen-capture";
 import Audio from "@uppy/audio";
 import XHR from "@uppy/xhr-upload";
+import Compressor, { CompressorOptions } from "@uppy/compressor";
 
 import "@uppy/core/dist/style.min.css";
 import "@uppy/dashboard/dist/style.min.css";
@@ -17,6 +18,11 @@ export const uppy = new Uppy()
   .use(Webcam)
   .use(ImageEditor, {})
   .use(ScreenCapture)
+  .use(Compressor, {
+    quality: 0.85,
+    convertSize: 128 * 1024,
+    mimeType: "image/webp",
+  } as CompressorOptions)
   .use(XHR, { endpoint: "" })
   .use(Audio);
 
